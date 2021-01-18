00df3dcf799e014a522e74cb2455160ea2e9e48c
Carlos Reyna <creyna.2m@gmail.com>
2021-01-17 21:05:26 -0300

    Added Microsoft's eshopOnContainers Event Bus library.

---
+++ src/BuildingBlocks/EventBus/EventBus.Tests/TestIntegrationEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Tests
+{
+    public class TestIntegrationEventHandler : IIntegrationEventHandler<TestIntegrationEvent>
+    {
+        public bool Handled { get; private set; }
+
+        public TestIntegrationEventHandler()
+        {
+            Handled = false;
+        }
+
+        public async Task Handle(TestIntegrationEvent @event)
+        {
+            await Task.CompletedTask;
+            Handled = true;
+        }
+    }
+}
